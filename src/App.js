8107537646959313d301105aee6e8b5f830ed655
pokiza1228 <baxromovapokiza1@gmail.com>
2022-05-06 18:22:18 +0500

    react-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-
+import LinkMain from "./components/main-link/main-link";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <LinkMain />
   );
 }
 
---
+++ src/components/main-link/main-link.js
@@ -0,0 +1,5 @@
+const LinkMain = () => {
+    return <a className="main-link">add Fedbeck</a>
+}
+
+export default LinkMain;
