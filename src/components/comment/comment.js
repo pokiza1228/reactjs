e5c6919f45e35635a48d14a5c3a83b58dbd2b122
pokiza1228 <baxromovapokiza1@gmail.com>
2022-05-12 08:48:47 +0500

    component radio

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -1,12 +1,12 @@
 import "./comment.scss"
 import Inner from "../inner/inner"
 import "../../assets/imges/icon.svg"
-
+import icon from "../../assets/imges/icon.svg"
 const Comment = ({span,text,h3,id,...props}) => {
     return (
         <li className="comment-item">
             <div className="commeny-item__nomber">
-                <img src="../../assets/imges/icon.svg" />
+                <img className="comment-item__icon" src={icon} />
                 <span>{id}</span>
             </div>
             <div className="comment-item__body">
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,11 @@
 import "./header.scss"
+import icon from "../../assets/imges/svet.svg"
 const { default: LinkMain } = require("../main-link/main-link")
 const Header = ({nomber, ...props}) => {
     return (
         <header className="header">
             <div className="header__content">
-                <img src="../../assets/imges/svet.svg"/>
+                <img src={icon}/>
                 <h2 className="header__title">{nomber} Suggestions</h2>
             </div>
             
